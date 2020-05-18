1bb4447718fa88c22a338434e9375fe532c52749
test <damianlukawski11@gmail.com>
2020-05-18 14:43:37 +0200

    some improvements

--- Weather_analysis.py
+++ Weather_analysis.py
@@ -19,7 +19,8 @@ with open('country-capitals.json', 'r', encoding="utf-8") as country_capitals:
 
 assert type(json_data1) == list
 list_of_capitals = [x['CapitalName'] for x in json_data1 if x['CapitalName'] != 'N/A']
-list_of_capitals.sort()
+list_of_countries = [x['CountryName'] for x in json_data1 if x['CapitalName'] != 'N/A']
+#list_of_capitals.sort()
 #print(list_of_capitals)
 
 #a=json.dumps(get_weather('Kair'), indent=2)
@@ -29,6 +30,7 @@ list_of_capitals.sort()
 
 list_of_holiday_locations = []
 list_of_holiday_temp = []
+list_of_holiday_countries = []
 i=0
 while i <len(list_of_capitals)-1:
     try:
@@ -36,18 +38,21 @@ while i <len(list_of_capitals)-1:
         if w['main']['temp'] > 15 and w['main']['temp'] < 25 and w['weather'][0]['description'] == "clear sky" and w['wind']['speed'] < 3:
             list_of_holiday_locations.append(w['name'])
             list_of_holiday_temp.append(w['main']['temp'])
+            list_of_holiday_countries.append(list_of_countries[i])
+            #print(list_of_holiday_countries)
         else:
             #print("bad weather conditions in %s" % (w['name']))
             "bad weather"
     except:
-        print("No city %s in our API database" %(list_of_capitals[i]))
+        "None"
+        #print("No city %s in our API database" %(list_of_capitals[i]))
     i += 1
 
-list_city_temp = zip(list_of_holiday_locations,list_of_holiday_temp)
+list_city_temp = zip(list_of_holiday_locations,list_of_holiday_temp, list_of_holiday_countries)
+list_city_temps = list(list_city_temp)
 print("if you wish to rest in a capital with temperature in range 15-25\N{DEGREE SIGN}C, no clouds and only slight wind please visit one of the following:")
-for city in list(list_city_temp):
-    i=1
-    print(str(i) + ". "+ city[0] + ", " + str(city[1])+ "\N{DEGREE SIGN}C")
-    i +=1
+for city in list_city_temps:
+    result = '%s (%s), %s \N{DEGREE SIGN}C' %(city[0], city[2], str(city[1]))
+    print(result)
 
 
